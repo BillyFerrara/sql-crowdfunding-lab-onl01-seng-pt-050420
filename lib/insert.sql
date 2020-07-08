01fafb01861840bb3ef76978ea1c225270820855
Billy Ferrara <willfira717@gmail.com>
2020-07-08 18:27:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,29 +1,29 @@
 # PROJECTS
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Chicken FBI", "Surveillance", 1000000, 1/1/17, 12/31/22);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Cups", "Cups", 10000000, 2/12/23. 12/2/24);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Cat Skateboarding Lessons", "Education", 99999999, 7/8/20, 12/31/99);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Taquitos for Tots", "Public Assistance", 3000000, 4/4/20, 9/9/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Batman inc", "Public Safety", 999999999, 3/17/37, 12/31/99);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Hunt War Criminals for Sport", "Vacation", 7777777777, 4/30/45, 4/30/46);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Bears...", "Animals", 7, 6/17/99, 9/23/23);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Kame House Training", "Fitness", 85555 , 2/26/86, 12/31/20);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Need Coffee", "Energy", 99999, 1/1/00, 12/31/99);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Banana Gram", "Messaging", 200000, 7/8/19, 9/9/20);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (1, "Chicken FBI", "Surveillance", 1000000, 1/1/17, 12/31/22);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (2, "Cups", "Cups", 10000000, 2/12/23. 12/2/24);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (3, "Cat Skateboarding Lessons", "Education", 99999999, 7/8/20, 12/31/99);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (4, "Taquitos for Tots", "Public Assistance", 3000000, 4/4/20, 9/9/20);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (5, "Batman inc", "Public Safety", 999999999, 3/17/37, 12/31/99);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (6, "Hunt War Criminals for Sport", "Vacation", 7777777777, 4/30/45, 4/30/46);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (7, "Bears...", "Animals", 7, 6/17/99, 9/23/23);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (8, "Kame House Training", "Fitness", 85555 , 2/26/86, 12/31/20);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (9, "Need Coffee", "Energy", 99999, 1/1/00, 12/31/99);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (10, "Banana Gram", "Messaging", 200000, 7/8/19, 9/9/20);
 
 # USERS
 
-INSERT INTO users (name, age)
+INSERT INTO users (id, name, age)
 VALUES ("Billy", 27);
 INSERT INTO users (name, age)
 VALUES ("Alyssa", 29);
