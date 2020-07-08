6572238d149bd195c4cd45cd410c3591f6aad51d
Billy Ferrara <willfira717@gmail.com>
2020-07-08 17:37:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("Chicken FBI", "Surveillance", 1000000, 1/1/17, 12/31/22);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ();
+VALUES ("Cups", "Cups", 10000000, 2/12/23. 12/2/24);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-\VALUES ();
+VALUES ("Cat Skateboarding Lessons", "Education", 99999999, 7/8/20, 12/31/99);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ();
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
