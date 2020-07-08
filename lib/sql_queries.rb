0dffd3f40952512f100d9734019e0ab077022459
Billy Ferrara <willfira717@gmail.com>
2020-07-08 18:49:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(projects.amount) FROM users INNER JOIN pledges ON users.id = pledges.user_id
-  GROUP BY users.name ORDER BY users.name;"
+  GROUP BY users.name ORDER BY SUM(prjects.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
