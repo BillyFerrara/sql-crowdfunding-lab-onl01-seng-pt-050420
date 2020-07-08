109ab276c0b3f3e4d4c7948ee195973950de2e7f
Billy Ferrara <willfira717@gmail.com>
2020-07-08 18:06:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,7 +19,7 @@ VALUES ("Need Coffee", "Energy", 99999, 1/1/00, 12/31/99);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("Banana Gram", "Messaging", 200000, 7/8/19, 9/9/20);
 
-INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES 
 ("Billy", 27)
 ("Alyssa", 29)
 ("Johnny", 10)
