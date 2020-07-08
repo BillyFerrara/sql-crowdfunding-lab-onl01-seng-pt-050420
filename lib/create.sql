fea183cee470ee0a9b6330202dc6b6fc5032d086
Billy Ferrara <willfira717@gmail.com>
2020-07-08 17:09:30 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,4 +2,4 @@ create table projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, fundin
 
 create table users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 
-create table pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER);
+create table pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
